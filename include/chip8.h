1878d83102bf6aaa007fc722f8ffc20165353a3a
Yash Patil <patilpyash@gmail.com>
2020-11-17 05:30:02 +0530

    'default-character-set-implemented'

--- include/chip8.h
+++ include/chip8.h
@@ -5,11 +5,17 @@
 #include "chip8memory.h"
 #include "chip8registers.h"
 #include "chip8stack.h"
+#include "chip8keyboard.h"
 
    struct chip8{
        struct chip8_stack stack;
        struct chip8_memory memory;
        struct chip8_registers registers;
+       struct chip8_keyboard keyboard;
    };
+
+
+void chip8_init(struct chip8* chip8);
+
 
 #endif
